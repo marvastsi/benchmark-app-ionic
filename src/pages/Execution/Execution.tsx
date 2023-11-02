2eaf1174ceca36667315f56fcb3ccaf19293d05f
Marcelo Ferreira Vasconcelos <marcelo.fvas@gmail.com>
2023-11-02 12:56:04 -0300

    wip: add react player successfuly

--- src/pages/Execution/Execution.tsx
+++ src/pages/Execution/Execution.tsx
@@ -1,9 +1,6 @@
-import { IonButton, IonContent, IonItem, IonNav, IonNavLink, IonText, IonTitle, useIonRouter } from '@ionic/react';
-import './Execution.css';
+import { IonButton, IonContent, IonItem, IonTitle, useIonRouter, withIonLifeCycle } from '@ionic/react';
 import AppBar from '../../components/AppBar';
-import Login from '../Login/LoginPage';
-import Account from '../Account/AccountPage';
-import { useHistory } from 'react-router';
+import './Execution.css';
 
 const Execution = () => {
   const ionRouter = useIonRouter();
@@ -48,24 +45,16 @@ const Execution = () => {
           >Upload</IonButton>
         </IonItem>
         <IonItem>
-        <IonButton
+          <IonButton
             onClick={(event) => {
               event.preventDefault();
               ionRouter.push("Media", "forward", "push");
             }}
           >Media</IonButton>
         </IonItem>
-        <IonItem>
-        <IonButton
-            onClick={(event) => {
-              event.preventDefault();
-              ionRouter.push("MediaPlayer", "forward", "push");
-            }}
-          >Media Player</IonButton>
-        </IonItem>
       </IonContent>
     </>
   );
 };
 
-export default Execution;
+export default withIonLifeCycle(Execution);
